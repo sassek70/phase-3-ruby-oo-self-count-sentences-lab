23222f9987ad1134457216b7d16950976a54313d
Kevin Sasse <sassek70@gmail.com>
2022-11-02 22:59:25 -0400

    updated alternate method for last test

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,20 +15,22 @@ class String
   end
 
   def count_sentences
-    sentences = 0
     punctuation = [".", "?", "!"]
     # checks each character individually and takes the index of that character to see if it matches a character
     # in the punctuation array. If it matches, and the value of the next index(character) does not match
     # any value in the punctuation array, it adds 1 to the total sentences.
-    self.chars.each.with_index do |char, index|
-      if punctuation.include?(char) && !punctuation.include?(self[index + 1])
-        sentences += 1
+    # self.chars.each.with_index do |char, index|
+    #   if punctuation.include?(char) && !punctuation.include?(self[index + 1])
+    #     sentences += 1
         # puts char
         # puts index
         # puts index+1
         # binding.pry
+
+      word_array = self.split(" ")
+      filter_word_array = word_array.filter do |word|
+        word.end_with?(".") || word.end_with?("?") || word.end_with?("!")
       end
-     end
-     sentences
+      filter_word_array.count
     end
 end
